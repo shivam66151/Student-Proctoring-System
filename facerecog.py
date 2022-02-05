c22210e05d49cff8d22bf7e0e389dd3725f472ca
Shivam Sharma <sharmashivam66151@gmail.com>
2022-02-05 14:08:48 +0000

    commited

---
+++ facerecog.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+import os
+import numpy as np
+
+# face detection
+
+img = cv.imread('')
+cv.imshow('',img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray People',gray)
