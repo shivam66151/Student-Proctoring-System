d3627f7fc0c126306d0b59ec0d645235e1693851
shivam66151 <sharmashivam66151@gmail.com>
2022-03-10 15:50:48 +0530

    file reading problem solved

--- Face authentication/face_auth.py
+++ Face authentication/face_auth.py
@@ -104,6 +104,34 @@ while True:
             if (count/rest_count) > 5 :
              print("Sucessfull Verification")
              sys.exit()
+    process_this_frame = not process_this_frame
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+     # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break   
+
+# Release handle to the webcam
+cap.release()
+cv2.destroyAllWindows() 
 
             
 
